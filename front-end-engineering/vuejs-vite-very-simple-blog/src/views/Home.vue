c6d3ca1d3e16d825fe7894bb08d9e26d25939f8c
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2025-06-22 12:38:53 -0300

    Build

--- front-end-engineering/vuejs-vite-very-simple-blog/src/views/Home.vue
+++ front-end-engineering/vuejs-vite-very-simple-blog/src/views/Home.vue
@@ -1,41 +1,26 @@
 <template>
   <div class="max-w-3xl mx-auto px-4 py-8">
     <h1 class="text-4xl font-bold mb-8 text-center text-gray-800">Meu Blog</h1>
+    <div v-if="!posts.length" class="text-center text-gray-500">
+      <p>Nenhum post encontrado.</p>
+    </div>
 
-    <!-- Posts da Página Atual -->
-    <PostCard
-      v-for="post in paginatedPosts"
-      :key="post.id"
-      :post="post"
-    />
+    <PostCard v-for="post in paginatedPosts" :key="post.id" :post="post" />
 
-    <!-- Paginação -->
     <div class="flex justify-center mt-8 space-x-2">
-      <button
-        class="px-3 py-1 border rounded hover:bg-gray-100"
-        :disabled="currentPage === 1"
-        @click="currentPage--"
-      >
+      <button class="px-3 py-1 border rounded hover:bg-gray-100" :disabled="currentPage === 1" @click="currentPage--">
         Anterior
       </button>
 
-      <button
-        v-for="page in totalPages"
-        :key="page"
-        @click="currentPage = page"
-        :class="[
-          'px-3 py-1 border rounded',
-          currentPage === page ? 'bg-blue-600 text-white' : 'hover:bg-gray-100'
-        ]"
-      >
+      <button v-for="page in totalPages" :key="page" @click="currentPage = page" :class="[
+        'px-3 py-1 border rounded',
+        currentPage === page ? 'bg-blue-600 text-white' : 'hover:bg-gray-100'
+      ]">
         {{ page }}
       </button>
 
-      <button
-        class="px-3 py-1 border rounded hover:bg-gray-100"
-        :disabled="currentPage === totalPages"
-        @click="currentPage++"
-      >
+      <button class="px-3 py-1 border rounded hover:bg-gray-100" :disabled="currentPage === totalPages"
+        @click="currentPage++">
         Próxima
       </button>
     </div>
